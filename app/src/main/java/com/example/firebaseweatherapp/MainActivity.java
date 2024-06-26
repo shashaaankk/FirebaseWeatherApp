a267f26e0c1f54667c5e7bf98f5f346bae3b29b1
shashaaankk <54775855+shashaaankk@users.noreply.github.com>
2024-06-26 15:33:32 +0200

    Getting Temperature and City Names

--- app/src/main/java/com/example/firebaseweatherapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseweatherapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.firebaseweatherapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+
+public class MainActivity extends AppCompatActivity {
+    Map<String, Object> results = new HashMap<>();
+    ArrayList<String> listItems;
+    private TextView display;
+    private String cityName;
+    private float currTemp;
+    private float prevTemp;
+    private float averageTemp;
+    ListView listView;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +39,49 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Setting up Buttons and Listeners
+        findViewById(R.id.updateTemp).setOnClickListener(v -> updateTempFirebase());    //Calls updateTempFirebase() on Click
+        findViewById(R.id.addCitybtn).setOnClickListener(v -> addCityFirebase());       //Calls addCityFirebase() on Click
+        //Display
+        display = findViewById(R.id.display);
+
+        //Initiate variables
+        cityName = "None";
+        currTemp = 0.0f;
+        averageTemp = 0.0f;
+
+        listItems = new ArrayList<>();
+    }
+
+    // Function to add city to the list of cities
+    private void addCityFirebase() {
+        //Toast.makeText(this,"Adding City", Toast.LENGTH_SHORT).show();
+        EditText newCity = findViewById(R.id.addCity);  //Getting City Name
+        cityName = newCity.getText().toString();
+        results.put("city", cityName);                  //For Displaying
+        updateDisplay();                                //Calling Display Function
+    }
+
+    // Function to update temperature of the selected city
+    private void updateTempFirebase() {
+        //Toast.makeText(this,"Updating Temperature", Toast.LENGTH_SHORT).show();
+        EditText newTemp = findViewById(R.id.Temp);    //Getting Temperature
+        try {                                          //handling blank input
+            currTemp = Float.parseFloat(newTemp.getText().toString());
+        }catch (NumberFormatException ex){
+            Toast.makeText(this,"Temperature must not be empty", Toast.LENGTH_SHORT).show();
+        }
+        results.put("temp", currTemp);                 //For Displaying
+        updateDisplay();                               //Calling Display Function
+    }
+
+    // Function to update Display
+    private void updateDisplay() {
+        StringBuilder displayText = new StringBuilder();
+        displayText.append("Selected City: ").append(results.get("city")).append("\n");
+        displayText.append("Temperature in °C: ").append(results.get("temp")).append("°C\n");
+        displayText.append("Average Temperature in °C: ").append(results.get("avgTemp")).append("°C\n");
+        display.setText(displayText.toString());
     }
 }
